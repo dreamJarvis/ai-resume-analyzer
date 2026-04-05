7cdc1c7b8900a13e40f3b6411560816ec2cf7c1a
Tarun Jha <jhatarun16@gmail.com>
2026-04-05 20:46:42 +0530

    added upload-form and file-uploader

---
+++ app/lib/utils.ts
@@ -0,0 +1,20 @@
+/** @format */
+
+export function formatSize(bytes: number): string {
+	if (bytes < 1024) {
+		return `${bytes} bytes`;
+	}
+
+	const kb = bytes / 1024;
+	if (kb < 1024) {
+		return `${kb.toFixed(1)} KB`;
+	}
+
+	const mb = kb / 1024;
+	if (mb < 1024) {
+		return `${mb.toFixed(1)} MB`;
+	}
+
+	const gb = mb / 1024;
+	return `${gb.toFixed(1)} GB`;
+}
